89d43d0e717f3ecb039c270deb404165a2191fbd
rahuls9568 <67452503+rahuls9568@users.noreply.github.com>
2020-06-29 15:59:31 +0530

    Update main.js

--- SkatenScratch/public/js/main.js
+++ SkatenScratch/public/js/main.js
@@ -24,7 +24,7 @@ var fontSettings = {
         scoreLifeText:"bold 40px Arial",
         codeText:"bold 20px Arial",
         UICamDiff:1,
-        thrustspeed: 0.044,
+        thrustspeed: 0.045,
         jumpSpeed:-1,
         gravity:0.002,
     },
@@ -33,7 +33,7 @@ var fontSettings = {
         scoreLifeText:"bold 30px Arial",
         codeText:"bold 20px Arial",
         UICamDiff:1,
-        thrustspeed: 0.044,
+        thrustspeed: 0.045,
         jumpSpeed:-0.85,
         gravity:0.002,
     } 
@@ -64,7 +64,7 @@ var config = {
     physics: {
         default: 'matter',
         matter: {
-            debug: false,
+            debug: true,
             gravity: {
                 x: 0,
                 y: 1
