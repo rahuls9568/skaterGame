c7750f4c759ecd7bda135be6fc1b96e6a9859dd1
rahuls9568 <67452503+rahuls9568@users.noreply.github.com>
2020-06-30 18:25:17 +0530

    Add files via upload

--- SkatenScratch/public/js/sceneInstruction.js
+++ SkatenScratch/public/js/sceneInstruction.js
@@ -24,7 +24,7 @@ class SceneInstruction extends Phaser.Scene
         this.cover = this.add.image(0,0,'insCover').setOrigin(0.5).setTint(0x555555);
         this.logo = this.add.image(0,0,'insLogo').setOrigin(0.5).setTint(0x555555);
         this.popup = this.add.image(0,0,'insPopup').setOrigin(0.5);
-        this.btn = this.add.image(0,0,'insBtn').setOrigin(0.5).setInteractive();
+        this.btn = this.add.image(0,0,'insBtn').setOrigin(0.5,0).setInteractive();
         //this.text = this.add.text(0,0,"CLICK TO START",{font:"bold 30px Arial",fill:"#FF0000",align:"center"}).setOrigin(0.5);
         this.isTouchFlag = false;
         
@@ -33,7 +33,9 @@ class SceneInstruction extends Phaser.Scene
 
         //this.agrid.placeAtIndex(60,this.bg);
         this.bg.setPosition(config.width/2,config.height);
-        Align.scaleToGameW(this.bg,1,this);
+        Align.scaleToGameH(this.bg,1,this);
+        if(currentFont == fontSettings.pc)
+            Align.scaleToGameW(this.bg,1,this);
         this.agrid.placeAtIndex(82,this.btn);
         Align.scaleToGameW(this.btn,0.3,this);
         this.agrid.placeAtIndex(49,this.cover);
@@ -42,6 +44,21 @@ class SceneInstruction extends Phaser.Scene
         Align.scaleToGameW(this.logo,0.3,this);
         this.agrid.placeAtIndex(60,this.popup);
         Align.scaleToGameW(this.popup,0.6,this);
+
+        var str = [
+            "Power up Cruncha Muncha by collecting the goodies along the way.",
+            "",
+            "Avoid the nasty obstacles that come your way. Like you, we too dislike oil, maida, and chemicals."
+        ]
+        
+        var graphics = this.make.graphics();
+	    // graphics.fillStyle(0xffffff);
+	    graphics.fillRect(this.popup.x-this.popup.displayWidth/2, this.popup.y-this.popup.displayHeight/2, this.popup.displayWidth, this.popup.displayHeight);
+	    var mask = new Phaser.Display.Masks.GeometryMask(this, graphics);
+        
+	    var insText= this.add.text(this.popup.x, this.popup.y+this.popup.displayHeight*0.03, str, { fontFamily:"myFont", fontSize:currentFont.instText, color: '#000000', align:"center", wordWrap: { width: this.popup.displayWidth*0.8 } }).setOrigin(0.5,0);
+	    insText.setMask(mask);
+
         
         this.btnInitScale = {x:this.btn.scaleX,y:this.btn.scaleY};
         this.btnOverScale = {x:this.btn.scaleX*0.9,y:this.btn.scaleY*0.9};
